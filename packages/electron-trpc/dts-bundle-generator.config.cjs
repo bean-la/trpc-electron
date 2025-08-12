8f0cde24588f5e6b0adca72fe6c7b90e5bf62f19
Sebastian Bean <seb@bean.la>
2025-08-11 22:57:58 -0700

    fix build

--- packages/electron-trpc/dts-bundle-generator.config.cjs
+++ packages/electron-trpc/dts-bundle-generator.config.cjs
@@ -1,19 +1,24 @@
-const config = {
+module.exports = {
   compilationOptions: {
     preferredConfigPath: './tsconfig.json',
+    followSymlinks: false,
   },
   entries: [
     {
       filePath: './src/renderer/index.ts',
       outFile: `./dist/renderer.d.ts`,
       noCheck: true,
+      libraries: {
+        inlinedLibraries: ['@trpc/client', '@trpc/server']
+      }
     },
     {
       filePath: './src/main/index.ts',
       outFile: `./dist/main.d.ts`,
       noCheck: true,
+      libraries: {
+        inlinedLibraries: ['@trpc/client', '@trpc/server']
+      }
     },
   ],
 };
-
-module.exports = config;
